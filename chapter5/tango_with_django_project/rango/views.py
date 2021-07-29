b6b8c99f4535e97a02b50ef24d0015dbb9ba9883
KaiyinisDad <88020704+KaiyinisDad@users.noreply.github.com>
2021-07-29 21:59:02 +0800

    Update views.py

--- chapter5/tango_with_django_project/rango/views.py
+++ chapter5/tango_with_django_project/rango/views.py
@@ -3,18 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     
-    # 构建一个字典，作为上下文传给模板引擎
-    # 注意，boldmessage 键对应于模板中的 {{ boldmessage }}
+   
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # 返回一个渲染后的响应发给客户端
-    # 为了方便，我们使用的是 render 函数的简短形式
-    # 注意，第二个参数是我们想使用的模板
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    # 构建一个字典，作为上下文传给模板引擎
-    # 注意，boldmessage 键对应于模板中的 {{ boldmessage }}
+   
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # 返回一个渲染后的响应发给客户端
-    # 为了方便，我们使用的是 render 函数的简短形式
-    # 注意，第二个参数是我们想使用的模板
     return render(request, 'rango/about.html', context=context_dict)
